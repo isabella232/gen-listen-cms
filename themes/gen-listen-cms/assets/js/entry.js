03aaa70b4bb8df92a6d62eefd94bd58678b947e6
DanKottke <dkottke@npr.org>
2016-05-26 14:09:21 -0400

    button fix, small fixes

--- themes/gen-listen-cms/assets/js/entry.js
+++ themes/gen-listen-cms/assets/js/entry.js
@@ -30,6 +30,7 @@
             e.preventDefault();
             $oldStation = $(this).parent().parent();
             $oldStation.find(".station-select-button").remove();
+            $oldStation.find(".station-select").append('&#x2713;');
             $('#station-search-result-area').empty();
             $('#station-search-result-area').html($oldStation);
             var orgId = $oldStation.find('.station-id').val();
