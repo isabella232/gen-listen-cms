9cdbc40af5aaa24bd41972971672c17737536eb7
DanKottke <dkottke@npr.org>
2016-05-26 17:33:12 -0400

    tumblr added

--- themes/gen-listen-cms/assets/js/social.js
+++ themes/gen-listen-cms/assets/js/social.js
@@ -13,8 +13,35 @@
         return false;
     });
 
+    $(".share-tumblr").click(function(e) {
+        e.stopPropagation();
+        var URL = "https://www.tumblr.com/widgets/share/tool/?posttype=photo";
+        var title = $('#share-title').val();
+        var text = $('#share-text').val();
+        var shareURL = $('#share-url').val();
+        var image = $('#share-image').val();
+
+        if (title) {
+            URL += "&title=" + title;
+        }
+        if (text) {
+            URL += "&caption=" + text;
+        }
+        if (image) {
+            URL += "&content=" + image;
+        }
+        if (shareURL) {
+            URL += "&photo-clickthru=" + shareURL;
+            URL += "&canonicalUrl=" + shareURL;
+        }
+
+        tabOpen(URL);
+        return false;
+    });
+
     var tabOpen = function(url) {
         window.open(url, '_blank', 'height=600,width=800,toolbar=no,directories=no,status=no, menubar=no,scrollbars=no,resizable=no ,modal=yes');
     };
+
 
 }(jQuery));
--- themes/gen-listen-cms/templates/Includes/Header.ss
+++ themes/gen-listen-cms/templates/Includes/Header.ss
@@ -6,5 +6,9 @@
         <a href="http://generationlisten.npr.org/"><img src="{$ThemeDir}/images/branding.png"/></a>
         </div>
     </div>
+    <input type="hidden" id="share-title" value="{$ShareTitle}" />
     <input type="hidden" id="share-text" value="{$ShareText}" />
+    <input type="hidden" id="share-url" value="{$ShareUrl}" />
+    <input type="hidden" id="share-image" value="{$ShareImageLink}" />
+    <input type="hidden" id="share-creator" value="{$ShareCreator}" />
 </header>
